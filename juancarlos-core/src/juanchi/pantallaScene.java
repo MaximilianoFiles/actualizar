702e93b8613c75845aec97050e6bc934b2c88dc8
Maximiliano <roberto@roberto-VirtualBox>
2015-05-02 23:11:30 +0200

    Segundo comit

--- juancarlos-core/src/juanchi/pantallaScene.java
+++ juancarlos-core/src/juanchi/pantallaScene.java
@@ -1,18 +1,45 @@
 package juanchi;
 
+import Actores.cocheActor;
+import Actores.fondoActor;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class pantallaScene extends AbstractScreen {
+	private Stage ESCENARIO;
+	private cocheActor PIBE;
+	private fondoActor FONDO;
 
 	public pantallaScene(principal Principal) {
 		super(Principal);
+		ESCENARIO = new Stage(new ScreenViewport());
+		Gdx.input.setInputProcessor(ESCENARIO);
+
+		FONDO = new fondoActor();
+		FONDO.setSize(ESCENARIO.getWidth(), ESCENARIO.getHeight());
+		ESCENARIO.addActor(FONDO);
+
+		PIBE = new cocheActor();
+		ESCENARIO.addActor(PIBE);
 
 	}
 
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0.2f, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		ESCENARIO.draw();
+		ESCENARIO.act();
+
+	}
+
+	public void resize() {
+		ESCENARIO.getViewport().update(Gdx.graphics.getWidth(),
+				Gdx.graphics.getHeight());
+		FONDO.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 	}
 
 }
